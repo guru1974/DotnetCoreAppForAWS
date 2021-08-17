df7c4cf4b8b92b281f9466803c6c6d0a53204f5d
Guru Seetharaman <guru.seetharaman@firstagain.local>
2021-08-17 11:14:49 -0700

    Add project files.

---
+++ DotnetCoreAppForAWS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotnetCoreAppForAWS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
